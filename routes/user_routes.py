6b1adafe3bf8dfd02dcb05061c62bc8a1ad754d9
praddyyyy <pradeeshxdev@gmail.com>
2022-08-18 11:03:06 +0530

    Added new route /sign-in

--- routes/user_routes.py
+++ routes/user_routes.py
@@ -1,7 +1,7 @@
 from email import message
 from fastapi import APIRouter
 
-from models.user_models import User
+from models.user_models import User, SignIn
 from config.db import collection
 from schemas.user_schemas import userEntity, usersEntity
 
@@ -26,7 +26,7 @@ async def get_user(id: str):
 @user.post('/create-user')
 async def create_user(user: User):
     _id = collection.insert_one(dict(user))
-    user = usersEntity(collection.find({"aadhar_no": id}))
+    user = usersEntity(collection.find({"_id": _id.inserted_id}))
     return {"status": "ok", "data": user}
 
 @user.put('/update-user-id/{id}')
@@ -39,5 +39,14 @@ async def update_user(id: str, user: User):
 
 @user.delete('/delete-user-id/{id}')
 async def delete_user(id: str):
-    collection.find_one_and_delete({"aadhar_no": id})
+    user = collection.find_one_and_delete({"aadhar_no": id})
     return {"status": "ok", "data": []}
+
+@user.post('/sign-in')
+async def sign_in(signIn: SignIn):
+    user = usersEntity(collection.find({"aadhar_no": signIn.aadhar_no, "email": signIn.email}))
+    if len(user)>0:
+        if user[0]['aadhar'] == signIn.aadhar_no and user[0]['email'] == signIn.email:
+            return {"success": True}
+    else:
+        return {"success": False}
