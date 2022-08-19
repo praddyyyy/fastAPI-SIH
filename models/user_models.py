a6e745533d5abf6a7e597abaae4c7c4cfe067c82
praddyyyy <pradeeshxdev@gmail.com>
2022-08-19 09:19:16 +0530

    Change in user model

--- models/user_models.py
+++ models/user_models.py
@@ -4,6 +4,18 @@ class User(BaseModel):
     name: str
     email: str
     aadhar_no: str
+    phone: str
+    birthdate: str
+    college: str
+    gender: str
+    course: str
+    admission: str
+    university: str
+    institute_type: str
+    institute_id: str
+    tc: str
+    doj: str
+    dol: str
 
 class SignIn(BaseModel):
     email: str
