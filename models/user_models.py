8562fd0d9ca6903061cd4bb9aada531553e46309
praddyyyy <pradeeshxdev@gmail.com>
2022-08-21 15:19:25 +0530

    Changes to BaseModel

--- models/user_models.py
+++ models/user_models.py
@@ -2,14 +2,12 @@ from pydantic import BaseModel
 from typing import Dict, List
 class AcademicDetail(BaseModel):
     college: str
-    university: str
     course: str
     doj: str
     dol: str
 class User(BaseModel):
     aadhar_no: str
     tc: str
-    institute_type: str
     institute_id: str
     name: str
     email: str
