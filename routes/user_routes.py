936d411d4db3bf470c43c2988f213be4632a3c57
praddyyyy <pradeeshxdev@gmail.com>
2022-08-26 03:29:06 +0530

    Minor Changes

--- routes/user_routes.py
+++ routes/user_routes.py
@@ -196,5 +196,5 @@ async def set_parent_approval(id:str, key:str):
 
 @user.get('/get-tc-approved-user')
 def get_tc_approved_user():
-    users = usersEntity(collection.find({"parent_approval": True}))
+    users = usersEntity(collection.find({"parent_approval": True, "tc": False}))
     return {"success": "ok", "data": users}
