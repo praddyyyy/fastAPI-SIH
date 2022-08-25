4760cdad73663e7c6bbf6dffe7c20478e65ece13
praddyyyy <pradeeshxdev@gmail.com>
2022-08-26 00:05:49 +0530

    Minor Changes

--- routes/user_routes.py
+++ routes/user_routes.py
@@ -1,11 +1,6 @@
 from fastapi import APIRouter
 import random
 
-import smtplib
-from email.mime.multipart import MIMEMultipart
-from email.mime.text import MIMEText
-
-
 from models.user_models import User, SignIn, Number, LoginIn, AadhaarUser, Email
 from config.db import collection, collection_institutes, collection_aadhaar
 from schemas.user_schemas import usersEntity, aadhaarsEntity
@@ -173,64 +168,3 @@ async def last(id:str, doj:str, dol:str):
 async def ekyc(aid):
     user = collection_aadhaar.find_one({"aadhaar": aid})
     return {"status": "ok", "data": {"name": user['name'], "phone": user['phone']}}
-
-TO = "pradeeshwar10@gmail.com"
-FROM = "mtech339@gmail.com"
-
-BODY = """
-
-<h1>Transfer Certificate Request Verification</h1>
-<a href="www.google.com">Authorize Request</a>
-<a href="www.google.com">Deny Request</a>
-
-"""
-
-def send_mail(TO, FROM, BODY):
-    # Create message container - the correct MIME type is multipart/alternative here!
-    MESSAGE = MIMEMultipart('alternative')
-    MESSAGE['subject'] = "SUBJECT"
-    MESSAGE['To'] = TO
-    MESSAGE['From'] = FROM
-    MESSAGE.preamble = """
-    Your mail reader does not support the report format.
-    Please visit us online!"""
-
-    # Record the MIME type text/html.
-    HTML_BODY = MIMEText(BODY, 'html')
-
-    # Attach parts into message container.
-    # According to RFC 2046, the last part of a multipart message, in this case
-    # the HTML message, is best and preferred.
-    MESSAGE.attach(HTML_BODY)
-
-    # The actual sending of the e-mail
-    server = smtplib.SMTP('smtp.gmail.com:587')
-
-    # Credentials (if needed) for sending the mail
-    password = "cmewcdgbvisfjfph"
-
-    server.starttls()
-    server.login(FROM,password)
-    server.sendmail(FROM, [TO], MESSAGE.as_string())
-    server.quit()
-
-    return True
-    
-@user.post('/send-mail-url/{mail}')
-async def send_mail_url(mail: str):
-    TO = mail
-    FROM = "mtech339@gmail.com"
-
-    BODY = """
-
-            <h1>Transfer Certificate Request Verification</h1>
-            <a href="www.google.com">Authorize Request</a>
-            <a href="www.google.com">Deny Request</a>
-
-            """
-    res = send_mail(TO, FROM, BODY)
-
-    if res == True:
-        return {"status": True}
-    else:
-        return {"status": False}
